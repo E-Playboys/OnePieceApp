e8eea012fa25ccd6d311794a1b6bfc64730dd957
Tri Vien <gachip11589@mgail.com>
2017-02-13 21:44:35 +0700

    Integrate Manga APIs

---
+++ OnePiece.App/OnePiece.App/Models/MangaImage.cs
@@ -0,0 +1,18 @@
+﻿namespace OnePiece.App.Models
+{
+    public class MangaImage
+    {
+        public string Name { get; set; }
+        public string Link { get; set; }
+        public bool IsLoading { get; set; }
+
+        public int Number { get; set; }
+        public string Description { get; set; }
+        public string AlternativeLink { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int ImageWidth { get; set; }
+
+        public int MangaChapterId { get; set; }
+    }
+}
---
+++ OnePiece.App/OnePiece.App/Services/Manga/GetVolumeRq.cs
@@ -0,0 +1,7 @@
+﻿namespace OnePiece.App.Services.Manga
+{
+    public class GetVolumeRq
+    {
+        public int VolumeId { get; set; }
+    }
+}
