0c879a87ef98b0cf79a9450f7cf9936c641b3993
Khang Tran <mr.khangtran@gmail.com>
2017-11-16 22:04:28 +0700

    data

--- Onepiece.VideoCollector/Program.cs
+++ Onepiece.VideoCollector/Program.cs
@@ -21,7 +21,11 @@ namespace Onepiece.VideoCollector
 
             if (action == "1")
             {
-                CollectFromFcOnePiece();
+                var driver = new FirefoxDriver { Url = "http://www.linkvip.info/" };
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+
+                //CollectFromFcOnePiece(driver);
+                CollectFromOnePieceFc(driver);
             }
             else if (action == "2")
             {
@@ -31,15 +35,12 @@ namespace Onepiece.VideoCollector
             }
         }
 
-        private static void CollectFromFcOnePiece()
+        private static void CollectFromFcOnePiece(FirefoxDriver driver)
         {
             var downloadLinkData = "";
 
             var web = new HtmlWeb();
             var htmlDoc = web.Load("http://fconepiece.com/one-piece-vietsub/").DocumentNode;
-
-            var driver = new FirefoxDriver { Url = "http://www.linkvip.info/" };
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
 
             var containerNode = htmlDoc.SelectSingleNode(@"//div[@class=""entry-content""]");
             foreach (var childNode in containerNode.ChildNodes)
@@ -88,8 +89,61 @@ namespace Onepiece.VideoCollector
             File.WriteAllText("LinkData.txt", downloadLinkData);
         }
 
+        private static void CollectFromOnePieceFc(FirefoxDriver driver)
+        {
+            var downloadLinkData = "";
+
+            var web = new HtmlWeb();
+            for (int i = 575; i <= 813; i++)
+            {
+                var htmlDoc = web.Load($"http://onepiecefc.com/vi/one-piece-anime/episode-{i}/").DocumentNode;
+
+                var fshareLinkNode = htmlDoc.SelectSingleNode(@"//a[contains(@title,'Fshare')]");
+                if (fshareLinkNode != null)
+                {
+                    driver.FindElement(By.Name("url")).Clear();
+                    driver.FindElement(By.Name("url")).SendKeys(fshareLinkNode.Attributes["href"].Value);
+                    driver.FindElement(By.TagName("button")).Click();
+                    var downloadLink = driver.FindElement(By.XPath("//h4/a")).GetAttribute("href");
+
+                    // if there is no file in fshare then fallback to zing video
+                    if (!downloadLink.Contains("fshare.vn"))
+                    {
+                        var iframeNode = htmlDoc.SelectSingleNode(@"//iframe");
+                        if (iframeNode == null)
+                        {
+                            continue;
+                        }
+
+                        var iframeDoc = web.Load(iframeNode.Attributes["src"].Value).DocumentNode;
+                        var videoNode = iframeDoc.SelectSingleNode(@"//video/source");
+
+                        downloadLink = videoNode.Attributes["src"].Value;
+                    }
+
+                    downloadLinkData += $"1_{i}_{downloadLink}" + Environment.NewLine;
+                }
+                else
+                {
+                    var iframeNode = htmlDoc.SelectSingleNode(@"//iframe");
+                    if (iframeNode == null)
+                    {
+                        continue;
+                    }
+
+                    var iframeDoc = web.Load(iframeNode.Attributes["src"].Value).DocumentNode;
+                    var videoNode = iframeDoc.SelectSingleNode(@"//video/source");
+
+                    downloadLinkData += $"1_{i}_{videoNode.Attributes["src"].Value}" + Environment.NewLine;
+                }
+            }
+
+            File.WriteAllText("LinkData_2.txt", downloadLinkData);
+        }
+
         private static void DownloadVideo()
         {
+            // data 1
             var tasks = new List<Task>();
             var linkData = File.ReadAllLines("LinkData.txt");
             foreach (var linkInfo in linkData)
@@ -101,6 +155,32 @@ namespace Onepiece.VideoCollector
                     using (WebClient wc = new WebClient())
                     {
                         wc.DownloadFile(new Uri(downloadLink), $@"Videos\{linkInfo.Split('_')[0]}_{linkInfo.Split('_')[1]}");
+                    }
+                });
+
+                tasks.Add(task);
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            // data 2
+            tasks = new List<Task>();
+            linkData = File.ReadAllLines("LinkData_2.txt");
+            foreach (var linkInfo in linkData)
+            {
+                // skip if there is no video to download
+                if (linkInfo.Length < 10)
+                {
+                    continue;
+                }
+
+                var downloadLink = linkInfo.Split('_')[2];
+
+                var task = Task.Run(() =>
+                {
+                    using (WebClient wc = new WebClient())
+                    {
+                        wc.DownloadFile(new Uri(downloadLink), $@"Videos\{linkInfo.Split('_')[0]}_{linkInfo.Split('_')[1]}.{linkInfo.Split('.').Last()}");
                     }
                 });
 
