cba3b90fb5e4ee02d9cb757830214790a7cafca5
Khang Tran <mr.khangtran@gmail.com>
2017-12-10 00:33:41 +0700

    download videos

--- Onepiece.VideoCollector/Program.cs
+++ Onepiece.VideoCollector/Program.cs
@@ -5,10 +5,13 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
 
 namespace Onepiece.VideoCollector
 {
@@ -19,19 +22,30 @@ namespace Onepiece.VideoCollector
             Console.Write("Collect (1) or Download (2): ");
             var action = Console.ReadLine();
 
+            FirefoxProfile profile = new FirefoxProfile();
+            profile.SetPreference("browser.download.dir", "D:\\Temp");  // folder
+            profile.SetPreference("browser.helperApps.neverAsk.saveToDisk", "video/mp4,application/x-troff-msvideo,video/avi,video/msvideo,video/x-msvideo,application/zip,application/x-rar-compressed,application/octet-stream");  // MIME type
+            profile.SetPreference("pdfjs.disabled", true);  // disable the built-in viewer
+            profile.SetPreference("browser.download.folderList", 2);
+            profile.SetPreference("browser.download.panel.shown", false);
+
+            //DesiredCapabilities capabilities = DesiredCapabilities.Firefox();
+            //capabilities.SetCapability(FirefoxDriver.ProfileCapabilityName, profile);
+            //capabilities.SetCapability(CapabilityType.ELEMENT_SCROLL_BEHAVIOR, 1);
+
+            var driver = new FirefoxDriver(profile);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+
             if (action == "1")
             {
-                var driver = new FirefoxDriver { Url = "http://www.linkvip.info/" };
-                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
-
-                //CollectFromFcOnePiece(driver);
+                CollectFromFcOnePiece(driver);
                 CollectFromOnePieceFc(driver);
             }
             else if (action == "2")
             {
                 Directory.CreateDirectory("Videos");
 
-                DownloadVideo();
+                DownloadVideo(driver);
             }
         }
 
@@ -63,22 +77,24 @@ namespace Onepiece.VideoCollector
                                 var linkWeb = new HtmlWeb();
                                 var linkHtmlDoc = linkWeb.Load(link).DocumentNode;
 
-                                string previousLink = null;
+                                //string previousLink = null;
                                 var epLinkNodes = linkHtmlDoc.SelectNodes(@"//a[@class=""filename""]");
                                 foreach (var epLinkNode in epLinkNodes)
                                 {
-                                    driver.FindElement(By.Name("url")).Clear();
-                                    driver.FindElement(By.Name("url")).SendKeys(epLinkNode.Attributes["href"].Value);
-                                    driver.FindElement(By.TagName("button")).Click();
-                                    var downloadLink = driver.FindElement(By.XPath("//h4/a")).GetAttribute("href");
+                                    //driver.FindElement(By.Name("url")).Clear();
+                                    //driver.FindElement(By.Name("url")).SendKeys(epLinkNode.Attributes["href"].Value);
+                                    //driver.FindElement(By.TagName("button")).Click();
+                                    //var downloadLink = driver.FindElement(By.XPath("//h4/a")).GetAttribute("href");
 
-                                    while (previousLink == downloadLink)
-                                    {
-                                        downloadLink = driver.FindElement(By.XPath("//h4/a")).GetAttribute("href");
-                                    }
+                                    //while (previousLink == downloadLink)
+                                    //{
+                                    //    downloadLink = driver.FindElement(By.XPath("//h4/a")).GetAttribute("href");
+                                    //}
 
-                                    previousLink = downloadLink;
-                                    downloadLinkData += $"{seasonNo}_{epLinkNode.InnerText}_{downloadLink}" + Environment.NewLine;
+                                    //previousLink = downloadLink;
+                                    //downloadLinkData += $"{seasonNo}_{epLinkNode.InnerText}_{downloadLink}" + Environment.NewLine;
+
+                                    downloadLinkData += $"{seasonNo}_{epLinkNode.InnerText}_{epLinkNode.Attributes["href"].Value}" + Environment.NewLine;
                                 }
                             }
                         }
@@ -101,27 +117,29 @@ namespace Onepiece.VideoCollector
                 var fshareLinkNode = htmlDoc.SelectSingleNode(@"//a[contains(@title,'Fshare')]");
                 if (fshareLinkNode != null)
                 {
-                    driver.FindElement(By.Name("url")).Clear();
-                    driver.FindElement(By.Name("url")).SendKeys(fshareLinkNode.Attributes["href"].Value);
-                    driver.FindElement(By.TagName("button")).Click();
-                    var downloadLink = driver.FindElement(By.XPath("//h4/a")).GetAttribute("href");
+                    //driver.FindElement(By.Name("url")).Clear();
+                    //driver.FindElement(By.Name("url")).SendKeys(fshareLinkNode.Attributes["href"].Value);
+                    //driver.FindElement(By.TagName("button")).Click();
+                    //var downloadLink = driver.FindElement(By.XPath("//h4/a")).GetAttribute("href");
 
-                    // if there is no file in fshare then fallback to zing video
-                    if (!downloadLink.Contains("fshare.vn"))
-                    {
-                        var iframeNode = htmlDoc.SelectSingleNode(@"//iframe");
-                        if (iframeNode == null)
-                        {
-                            continue;
-                        }
+                    //// if there is no file in fshare then fallback to zing video
+                    //if (!downloadLink.Contains("fshare.vn"))
+                    //{
+                    //    var iframeNode = htmlDoc.SelectSingleNode(@"//iframe");
+                    //    if (iframeNode == null)
+                    //    {
+                    //        continue;
+                    //    }
 
-                        var iframeDoc = web.Load(iframeNode.Attributes["src"].Value).DocumentNode;
-                        var videoNode = iframeDoc.SelectSingleNode(@"//video/source");
+                    //    var iframeDoc = web.Load(iframeNode.Attributes["src"].Value).DocumentNode;
+                    //    var videoNode = iframeDoc.SelectSingleNode(@"//video/source");
 
-                        downloadLink = videoNode.Attributes["src"].Value;
-                    }
+                    //    downloadLink = videoNode.Attributes["src"].Value;
+                    //}
 
-                    downloadLinkData += $"1_{i}_{downloadLink}" + Environment.NewLine;
+                    //downloadLinkData += $"1_{i}_{downloadLink}" + Environment.NewLine;
+
+                    downloadLinkData += $"1_{i}_{fshareLinkNode.Attributes["href"].Value}" + Environment.NewLine;
                 }
                 else
                 {
@@ -141,53 +159,109 @@ namespace Onepiece.VideoCollector
             File.WriteAllText("LinkData_2.txt", downloadLinkData);
         }
 
-        private static void DownloadVideo()
+        private static void DownloadVideo(FirefoxDriver driver)
         {
+            driver.Url = "https://www.fshare.vn/";
+            driver.FindElementsById("LoginForm_email")[1].Clear();
+            driver.FindElementsById("LoginForm_email")[1].SendKeys("troyuit@gmail.com");
+            driver.FindElementsById("LoginForm_password")[1].Clear();
+            driver.FindElementsById("LoginForm_password")[1].SendKeys("thuyphuong");
+            driver.FindElementByName("yt0").Click();
+
+            Thread.Sleep(TimeSpan.FromSeconds(10));
+
             // data 1
-            var tasks = new List<Task>();
+            //var tasks = new List<Task>();
             var linkData = File.ReadAllLines("LinkData.txt");
-            foreach (var linkInfo in linkData)
+            var totalCount = linkData.Count();
+            var pageSize = 10;
+            var pageCount = (totalCount + pageSize - 1) / pageSize;
+
+            for (int i = 0; i <= pageCount; i++)
             {
-                var downloadLink = linkInfo.Split('_')[2];
-
-                var task = Task.Run(() =>
+                var links = linkData.Skip(i * pageCount).Take(pageSize);
+                foreach (var link in links)
                 {
-                    using (WebClient wc = new WebClient())
-                    {
-                        wc.DownloadFile(new Uri(downloadLink), $@"Videos\{linkInfo.Split('_')[0]}_{linkInfo.Split('_')[1]}");
-                    }
-                });
-
-                tasks.Add(task);
+                    var downloadLink = link.Split('_')[2];
+                    driver.Url = downloadLink;
+                    driver.FindElementByClassName("btn-download-vip").Click();
+                }
             }
 
-            Task.WaitAll(tasks.ToArray());
+            //foreach (var linkInfo in linkData)
+            //{
+                //var downloadLink = linkInfo.Split('_')[2];
+
+                //var task = Task.Run(() =>
+                //{
+                //    using (WebClient wc = new WebClient())
+                //    {
+                //        wc.DownloadFile(new Uri(downloadLink), $@"Videos\{linkInfo.Split('_')[0]}_{linkInfo.Split('_')[1]}");
+                //    }
+                //});
+
+                //tasks.Add(task);
+            //}
+
+            //Task.WaitAll(tasks.ToArray());
 
             // data 2
-            tasks = new List<Task>();
+            var tasks = new List<Task>();
             linkData = File.ReadAllLines("LinkData_2.txt");
-            foreach (var linkInfo in linkData)
+            totalCount = linkData.Count();
+            pageSize = 10;
+            pageCount = (totalCount + pageSize - 1) / pageSize;
+
+            for (int i = 0; i <= pageCount; i++)
             {
-                // skip if there is no video to download
-                if (linkInfo.Length < 10)
+                var links = linkData.Skip(i * pageCount).Take(pageSize);
+                foreach (var link in links)
                 {
-                    continue;
+                    var downloadLink = link.Split('_')[2];
+                    if (downloadLink.Contains("fshare"))
+                    {
+                        driver.Url = downloadLink;
+                        driver.FindElementByClassName("btn-download-vip").Click();
+                    }
+                    else
+                    {
+                        var task = Task.Run(() =>
+                        {
+                            using (var wc = new WebClient())
+                            {
+                                wc.DownloadFile(new Uri(downloadLink), $@"Videos\{link.Split('_')[0]}_{link.Split('_')[1]}.{link.Split('.').Last()}");
+                            }
+                        });
+
+                        tasks.Add(task);
+                    }
                 }
 
-                var downloadLink = linkInfo.Split('_')[2];
-
-                var task = Task.Run(() =>
-                {
-                    using (WebClient wc = new WebClient())
-                    {
-                        wc.DownloadFile(new Uri(downloadLink), $@"Videos\{linkInfo.Split('_')[0]}_{linkInfo.Split('_')[1]}.{linkInfo.Split('.').Last()}");
-                    }
-                });
-
-                tasks.Add(task);
+                Task.WaitAll(tasks.ToArray());
             }
 
-            Task.WaitAll(tasks.ToArray());
+            //foreach (var linkInfo in linkData)
+            //{
+            // skip if there is no video to download
+            //if (linkInfo.Length < 10)
+            //{
+            //    continue;
+            //}
+
+            //var downloadLink = linkInfo.Split('_')[2];
+
+            //var task = Task.Run(() =>
+            //{
+            //    using (WebClient wc = new WebClient())
+            //    {
+            //        wc.DownloadFile(new Uri(downloadLink), $@"Videos\{linkInfo.Split('_')[0]}_{linkInfo.Split('_')[1]}.{linkInfo.Split('.').Last()}");
+            //    }
+            //});
+
+            //tasks.Add(task);
+            //}
+
+            //Task.WaitAll(tasks.ToArray());
         }
     }
 }
